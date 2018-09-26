6d5f4f32dc299fe4ab54efbbb648726df314063b
Venelin Manchev <venelinphp@gmail.com>
2018-09-26 20:40:27 +0300

    feat: responsive sectors, slots and header

--- src/app/toxic-schedule-header/toxic-schedule-header.component.ts
+++ src/app/toxic-schedule-header/toxic-schedule-header.component.ts
@@ -1,11 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'toxic-schedule-header',
-  templateUrl: './toxic-schedule-header.component.html',
-  styleUrls: ['./toxic-schedule-header.component.scss']
+  selector: "toxic-schedule-header",
+  templateUrl: "./toxic-schedule-header.component.html",
+  styleUrls: ["./toxic-schedule-header.component.scss"]
 })
 export class ToxicScheduleHeaderComponent {
   @Input()
   sectors: string[];
+  @Output()
+  txSelectedSector = new EventEmitter<String>();
+
+  txSetSector($event) {
+    this.txSelectedSector.emit($event.target.value);
+  }
 }
