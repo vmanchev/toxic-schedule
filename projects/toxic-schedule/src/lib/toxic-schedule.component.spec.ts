a9dae7a833577bba73bc0de41099bdfd6b7c2187
Venelin Manchev <vmanchev@soft2run.com>
2018-10-01 14:24:02 +0300

    test: initial commit

--- projects/toxic-schedule/src/lib/toxic-schedule.component.spec.ts
+++ projects/toxic-schedule/src/lib/toxic-schedule.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ToxicScheduleComponent } from './toxic-schedule.component';
+import { ToxicScheduleHeaderComponent } from './toxic-schedule-header/toxic-schedule-header.component';
+import { ToxicScheduleGridComponent } from './toxic-schedule-grid/toxic-schedule-grid.component';
 
 describe('ToxicScheduleComponent', () => {
   let component: ToxicScheduleComponent;
@@ -8,14 +10,22 @@ describe('ToxicScheduleComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ToxicScheduleComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        ToxicScheduleComponent,
+        ToxicScheduleHeaderComponent,
+        ToxicScheduleGridComponent
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ToxicScheduleComponent);
     component = fixture.componentInstance;
+    component.interval = 5;
+    component.startTime = new Date('2018-05-20T10:00:00');
+    component.endTime = new Date('2018-05-20T12:00:00');
+    component.reservedSlots = [];
+    component.sectors = ['sector1', 'sector2'];
     fixture.detectChanges();
   });
 
