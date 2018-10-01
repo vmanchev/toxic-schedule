02be83a8a92ca56b0b7daea602b285481fc86f03
Venelin Manchev <vmanchev@soft2run.com>
2018-10-01 14:28:59 +0300

    app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
+import { ToxicScheduleModule } from '../../projects/toxic-schedule/src/lib/toxic-schedule.module';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [ToxicScheduleModule]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
